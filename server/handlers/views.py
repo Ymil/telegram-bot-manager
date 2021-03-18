5e74052578f5d7f1c0947a46699254973899df5e
Lautaro Linquiman <lylinquiman@gmail.com>
2021-03-17 21:31:49 -0300

    Add validation for duplicated commands

--- server/handlers/views.py
+++ server/handlers/views.py
@@ -1,9 +1,9 @@
 import django_filters
 from handlers.models import Handler
 from handlers.serializers import HandlerSerializer
+from django.http import JsonResponse
 from rest_framework import viewsets
 from rest_framework import filters
-
 
 class HandlerViewSet(viewsets.ModelViewSet):
     """
@@ -13,6 +13,12 @@ class HandlerViewSet(viewsets.ModelViewSet):
     serializer_class = HandlerSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.OrderingFilter]
     ordering = ['pk']
+
+    def create(self, request, *args, **kwargs):
+        command = request.data['command']
+        if Handler.objects.filter(command=command):            
+            return JsonResponse(status=412, data={"description": f"Already registered handler with command {command}"})
+        return super().create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
         kwargs['partial'] = True
